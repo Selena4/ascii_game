8182b7c1f2c586df66a871c49ef507962c3e4437
Selena4 <43804966+Selena4@users.noreply.github.com>
2019-12-10 19:54:51 +0300

    Add files via upload

--- core/actions.py
+++ core/actions.py
@@ -8,7 +8,7 @@ class game:
 		stat = json.loads(open('resources/restart_chr','r').read())
 		with open("resources/character", "w") as write_file:
 			json.dump(stat, write_file)
-	def show_table(table,overview,x,y):
+	def show_table(table,overview,x,y,name, icon, class_, level, exp, max_exp, hp, hp_max):
 		if y - (overview//2) < 0:
 			y = overview//2 
 		if x - (overview//2) < 0:
@@ -17,18 +17,29 @@ class game:
 			x = len(table[y]) - overview//2 
 		if y + (overview//2) > len(table) -1: 
 			y = len(table) - overview//2
+		print('-'*((overview//2)*2+5))
+		print('|' + ' '*((overview//2)*2 + 3) + '|      [' + icon + '] '+ name + ' ' + str(level) + ' lvl (' + str(hp) + '/' + str(hp_max) + ' hp)')
+		t = 0
 		for line in table[y-(overview//2):y+(overview//2)+1]:
-			print(line[x-(overview//2):x+(overview//2)+1])
+			if t == 1:
+				print('| ' + line[x-(overview//2):x+(overview//2)+1] + ' |      class: ' + class_)
+			elif t == 3:
+				print('| ' + line[x-(overview//2):x+(overview//2)+1] + ' |      exp: ' + str(exp) + ' / ' + str(max_exp))
+			else:
+				print('| ' + line[x-(overview//2):x+(overview//2)+1] + ' |')
+			t += 1
+		print('|' + ' '*((overview//2)*2 + 3) + '|')
+		print('-'*((overview//2)*2+3)+'MAP')
 	def set_marker(marker,table,x,y):
 		table[y] = table[y][:x] + marker + table[y][x+1:]
 		return table
-	def key(key,floor,x,y,last_sym):
+	def key(key,floor,x,y,last_sym,icon):
 		if key  == 'W':
 			if y - 1  < 0: 
 				print('none')
 				return floor,x,y,last_sym
 			sym = floor[y-1][x]  
-			game.set_marker('@',floor,x,y-1)
+			game.set_marker(icon,floor,x,y-1)
 			game.set_marker(last_sym,floor,x,y)
 			return floor, x,y-1,sym
 		if key  == 'D':
@@ -36,7 +47,7 @@ class game:
 				print('none')
 				return floor,x,y,last_sym
 			sym = floor[y][x+1]  
-			game.set_marker('@',floor,x+1,y)
+			game.set_marker(icon,floor,x+1,y)
 			game.set_marker(last_sym,floor,x,y)
 			return floor, x+1,y,sym
 		if key  == 'A':
@@ -44,7 +55,7 @@ class game:
 				print('none')
 				return floor,x,y,last_sym
 			sym = floor[y][x-1]  
-			game.set_marker('@',floor,x-1,y)
+			game.set_marker(icon,floor,x-1,y)
 			game.set_marker(last_sym,floor,x,y)
 			return floor, x-1,y,sym
 		if key  == 'S':
@@ -52,6 +63,6 @@ class game:
 				print('none')
 				return floor,x,y,last_sym
 			sym = floor[y+1][x]  
-			game.set_marker('@',floor,x,y+1)
+			game.set_marker(icon,floor,x,y+1)
 			game.set_marker(last_sym,floor,x,y)
 			return floor, x,y+1,sym
--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def main():
 		stat = select_class.char.start_()
 		action.game.save_char(stat)
 	floor_now,x_pl,y_pl = floor.level.get_lvl(stat['floor'])[0], floor.level.get_lvl(stat['floor'])[1], floor.level.get_lvl(stat['floor'])[2]
-	floor_now = action.game.set_marker('@',floor_now,x_pl,y_pl)
+	floor_now = action.game.set_marker(stat['icon'],floor_now,x_pl,y_pl)
 	while True:
 		os.system('cls')
-		action.game.show_table(floor_now,stat['overview'],x_pl,y_pl)
-		floor_now,x_pl,y_pl, sym_now = action.game.key(getch.getch_().decode('utf-8').upper(),floor_now,x_pl,y_pl, sym_now)
+		action.game.show_table(floor_now,stat['overview'],x_pl,y_pl,stat['name'],stat['icon'],stat['class'],stat['level'],stat['exp'],stat['max_exp'],stat['hp'],stat['hp_max']) #name, icon, class_, level, exp, max_exp, hp, hp_max
+		floor_now,x_pl,y_pl, sym_now = action.game.key(getch.getch_().decode('utf-8').upper(),floor_now,x_pl,y_pl, sym_now,stat['icon'])
 if __name__ == "__main__":
 	main()
