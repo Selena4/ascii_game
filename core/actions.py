0633d6ed2b88b916028a27477a3f0031ba3cd7c2
Selena4 <43804966+Selena4@users.noreply.github.com>
2019-12-11 23:28:24 +0300

    Add files via upload

--- core/actions.py
+++ core/actions.py
@@ -1,14 +1,18 @@
-import json
+import json,os, sys,time
+import core.getch as getch
 
 class game:
 	def save_char(stat):
-		with open("resources/character", "w") as write_file:
+		with open("resources/save/character", "w") as write_file:
 			json.dump(stat, write_file)
 	def restart_game():
 		stat = json.loads(open('resources/restart_chr','r').read())
-		with open("resources/character", "w") as write_file:
+		with open("resources/save/character", "w") as write_file:
 			json.dump(stat, write_file)
-	def show_table(table,overview,x,y,name, icon, class_, level, exp, max_exp, hp, hp_max):
+	def relive():
+		os.system('cls')
+		input('oh sorry man. as I see, you are dead, but don’t worry, it’s temporary')
+	def show_table(table,overview,x,y,name, icon, class_, level, exp, max_exp, hp, hp_max, gold, water, oxy,text):
 		x_pl = x
 		y_pl = y
 		if y - (overview//2) < 0:
@@ -19,55 +23,79 @@ class game:
 			x = len(table[y]) - overview//2 -1
 		if y + (overview//2) > len(table) -1: 
 			y = len(table) - overview//2 -1
-		print('')
-		print(' ' +'-'*((overview//2)*2+5))
-		print(' |' + ' '*((overview//2)*2 + 3) + '|      [' + icon + '] '+ name + ' ' + str(level) + ' lvl (' + str(hp) + '/' + str(hp_max) + ' hp)')
+		output = '\r\n ' +'-'*((overview//2)*2+5) + '\n'
+		output = output +' |' + ' '*((overview//2)*2 + 3) + '|      [' + icon + '] '+ name + ' ' + str(level) + ' lvl (' + str(hp) + '/' + str(hp_max) + ' hp) | gold: ' + str(gold)  + '\n'
 		t = 0
 		for line in table[y-(overview//2):y+(overview//2)+1]:
 			if t == 1:
-				print(' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      class: ' + class_)
+				output = output +' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      class: ' + class_ + '\n'
 			elif t == 3:
-				print(' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      exp: ' + str(exp) + ' / ' + str(max_exp))
+				output = output + ' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      exp: ' + str(exp) + ' / ' + str(max_exp) + '\n'
 			elif t == 5: 
-				print(' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      x: ' + str(x_pl+1) + ', y:' + str(y_pl+1))
+				output = output +' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      x: ' + str(x_pl+1) + ', y:' + str(y_pl+1) + '\n'
+			elif t == 7:
+				output = output + ' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      key \'H\' - show helping table \n'
+			elif t == 9 and water:
+				output = output + ' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |      oxygen: ' + str(oxy) + '\n'
 			else:
-				print(' | ' + line[x-(overview//2):x+(overview//2)+1] + ' |')
+				output = output +' | ' + line[x-(overview//2):x+(overview//2)+1] + ' | \n'
 			t += 1
-		print(' |' + ' '*((overview//2)*2 + 3) + '|')
-		print(' ' +'-'*((overview//2)*2+3)+'MAP')
+		output = output + ' |' + ' '*((overview//2)*2 + 3) + '|\n'
+		output = output + ' ' +'-'*((overview//2)*2+3)+'MAP\n'
+		output = output + '\n' +text
+		sys.stdout.write(output)
+		sys.stdout.flush()
 	def set_marker(marker,table,x,y):
 		table[y] = table[y][:x] + marker + table[y][x+1:]
 		return table
-	def key(key,floor,x,y,last_sym,icon):
+	def move(key,floor,x,y,last_sym,icon):
+		objects_sym = ['/','\\','-','_', '|']
 		if key  == 'W':
-			if y - 1  < 0: 
-				print('none')
-				return floor,x,y,last_sym
+			if y - 1  < 0 or floor[y-1][x] in objects_sym: 
+				return floor,x,y,last_sym, 'none'
 			sym = floor[y-1][x]  
 			game.set_marker(icon,floor,x,y-1)
 			game.set_marker(last_sym,floor,x,y)
-			return floor, x,y-1,sym
+			return floor, x,y-1,sym, ''
 		if key  == 'D':
-			if x + 1  > len(floor[y])-1:
-				print('none')
-				return floor,x,y,last_sym
+			if x + 1  > len(floor[y])-1 or floor[y][x+1] in objects_sym:
+				return floor,x,y,last_sym, 'none'
 			sym = floor[y][x+1]  
 			game.set_marker(icon,floor,x+1,y)
 			game.set_marker(last_sym,floor,x,y)
-			return floor, x+1,y,sym
+			return floor, x+1,y,sym, ''
 		if key  == 'A':
-			if x - 1  < 0: 
-				print('none')
-				return floor,x,y,last_sym
+			if x - 1  < 0 or floor[y][x-1] in objects_sym: 
+				return floor,x,y,last_sym, 'none'
 			sym = floor[y][x-1]  
 			game.set_marker(icon,floor,x-1,y)
 			game.set_marker(last_sym,floor,x,y)
-			return floor, x-1,y,sym
+			return floor, x-1,y,sym, ''
 		if key  == 'S':
-			if y + 1  > len(floor)-1: 
-				print('none')
-				return floor,x,y,last_sym
+			if y + 1  > len(floor)-1 or floor[y+1][x] in objects_sym: 
+				return floor,x,y,last_sym, 'none'
 			sym = floor[y+1][x]  
 			game.set_marker(icon,floor,x,y+1)
 			game.set_marker(last_sym,floor,x,y)
-			return floor, x,y+1,sym
+			return floor, x,y+1,sym, ''
+	def use(key, stat):
+		if key == "H":
+			os.system('cls')
+			print('* L - legend of map')
+			print('* I - show inventory')
+			print('* Z - exit with save')
+			print('* [ - use potion of health')
+			print('* press any key')
+			getch.getch()
+		if key == "Z":
+			os.system('cls')
+			if input('are you sure?[y/n]').lower() == 'y':
+				game.save_char(stat)
+				exit()
+			else:
+				return
+		if key == 'I':
+			print('inventory:\n')
+			for item in stat['inventory']:
+				print(item)
+			getch.getch()
--- main.py
+++ main.py
@@ -3,12 +3,17 @@ import resources.floors as floor
 import core.start as select_class
 import core.actions as action
 import core.getch as getch
-import json, os,time
+import json, os,time, sys
 """_________________________________________"""
 
+#I recommend you run this file with cmd or terminal otherwise you can get errors
+
 def main():
+	water = False
+	oxy = 30
 	sym_now = '.'
-	stat = json.loads(open('resources/character','r').read())
+	text = ''
+	stat = json.loads(open('resources/save/character','r').read())
 	if stat['name'] == '':
 		stat = select_class.char.start_()
 		action.game.save_char(stat)
@@ -16,7 +21,29 @@ def main():
 	floor_now = action.game.set_marker(stat['icon'],floor_now,x_pl,y_pl)
 	while True:
 		os.system('cls')
-		action.game.show_table(floor_now,stat['overview'],x_pl,y_pl,stat['name'],stat['icon'],stat['class'],stat['level'],stat['exp'],stat['max_exp'],stat['hp'],stat['hp_max']) #name, icon, class_, level, exp, max_exp, hp, hp_max
-		floor_now,x_pl,y_pl, sym_now = action.game.key(getch.getch_().decode('utf-8').upper(),floor_now,x_pl,y_pl, sym_now,stat['icon'])
+		action.game.show_table(floor_now,stat['overview'],x_pl,y_pl,stat['name'],stat['icon'],stat['class'],stat['level'],stat['exp'],stat['max_exp'],stat['hp'],stat['hp_max'], stat['gold'], water,oxy, text) #name, icon, class_, level, exp, max_exp, hp, hp_max
+		time.sleep(1/(stat['dex']*3))
+		key = getch.getch_().decode('utf-8').upper()
+		if key in ['W','A','S','D']:
+			floor_now,x_pl,y_pl, sym_now, text = action.game.move(key,floor_now,x_pl,y_pl, sym_now,stat['icon'])
+		else:
+			action.game.use(key,stat)
+		if sym_now == "~":
+			water = True
+			if oxy > 0:
+				oxy -= 1
+			if oxy == 0:
+				stat['hp'] = stat['hp'] - 5
+		else:
+			water = False
+			if oxy < 30:
+				oxy += 1
+		if stat['hp'] <= 0:
+			action.game.relive()
+			sym_now = ' '
+			stat['hp'] = stat['hp_max']
+			floor_now,x_pl,y_pl = floor.level.get_lvl(stat['floor'])[0], floor.level.get_lvl(stat['floor'])[1], floor.level.get_lvl(stat['floor'])[2]
+			floor_now = action.game.set_marker(stat['icon'],floor_now,x_pl,y_pl)
+
 if __name__ == "__main__":
 	main()
